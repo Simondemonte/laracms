745f7540278ba810faf27b157f7b5f60bf7c009f
Simondemonte <Simondemonte8@gmail.com>
2017-09-12 04:04:21 +0700

    Level 8
    Update Data

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -36,6 +36,15 @@ Route::get('basicinsert', function(){
 Route::get('/create', function(){
 	Post::create(['title'=>'create method', 'content'=> 'saya belajar banyak hari ini']);
 });
+
+Route::get('/basicupdate', function(){
+		$post = Post::find(2);
+		
+		$post->title = 'Updated Eloquent Title';
+		$post->content = 'Updated Eloquent Content';
+		
+		$post->save();
+});
 //Route::get('/', function () {
 
 //    return view('welcome');
